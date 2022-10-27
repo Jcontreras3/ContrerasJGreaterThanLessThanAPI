06ec552b81d0d4c1cbc85ebd4be3e26fa83eaed4
Jovann Contreras <jcontreras@codestack.co>
2022-10-27 13:12:42 -0700

    git hub

---
+++ Controllers/GreaterThanLessThanController.cs
@@ -0,0 +1,43 @@
+//Jovann Contreras
+//Date 10/25/2022
+//Peer reviewed by: Griffin Parker - The code looks and runs great!! I'm not sure what models are, but they are obviously working in the program
+//Now I have something to look in to when I'm at home today. Great job!!
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GreatLess.Model;
+
+namespace ContrerasJGreaterThanLessThanAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreaterThanLessThanController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GreatorLess")]
+
+        public string GreaterThanLessThanChallenge(GreatLessModel item)
+        {
+            string Answer = "";
+            if (item.num1 > item.num2)
+            {
+                Answer = $"{item.num1} is greater than {item.num2}";
+            }
+            else if (item.num1 < item.num2)
+            {
+                Answer = $"{item.num1} is less than {item.num2}";
+            }
+            else
+            {
+                Answer = $"{item.num1} is equal to {item.num2}";
+            }
+
+            return Answer;
+        }
+    }
+}
---
+++ Models/GreatLessModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreatLess.Model
+{
+    public class GreatLessModel
+    {
+        public int num1 {get; set;}
+        public int num2 {get; set;}
+        //public string Answer {get; set;}
+        public GreatLessModel(){}
+
+    }
+}
